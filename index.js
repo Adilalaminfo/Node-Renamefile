56177739aeccfc9e3c893441d86251a7355016f8
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-27 17:13:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs=require('fs');
+
+// fs.writeFileSync("Read.pdf","My name is Md Adil Alam");
+
+// fs.appendFileSync("Read.pdf", "Hello how are you I am fine thank you What about you");
+
+fs.renameSync("ReadWrite.txt","ReadWrite1.pdf");
